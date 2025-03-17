724133e91e5e464a079cbdca1a9fb29c354867ca
илья <Oksimonov@mail.ru>
2025-03-17 20:46:38 +0300

    Добавление конструктора, конструктора копирования и деструктора

--- module_2/lab_1/lab_1.cpp
+++ module_2/lab_1/lab_1.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
 #include <stdexcept>
+#include <vector>
+
 
 using namespace std;
+
+
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+
+    Node(int k) : key(k), left(nullptr), right(nullptr) {}
+};
+
+
+class BinarySearchTree {
+    private:
+    Node* root;
+
+    //Вспомогательные функции
+    Node* copyTree(Node* node) {
+        if (node == nullptr) return nullptr;
+        Node* newNode = new Node(node->key);
+        newNode->left = copyTree(node->left);
+        newNode->right = copyTree(node->right);
+        return newNode;
+    }
+
+    void destroyTree(Node* node) {
+        if (node) {
+            destroyTree(node->left);
+            destroyTree(node->right);
+            delete node;
+        }
+    }
+
+
+    public:
+    BinarySearchTree() : root(nullptr) {}
+
+    BinarySearchTree(const BinarySearchTree& other) {
+        root = copyTree(other.root);
+    }
+
+    ~BinarySearchTree() {
+        destroyTree(root);
+    }
+
+
+
+
+
+    
+
+};
 
 int main() {
     setlocale(LC_ALL, "ru_RU");
