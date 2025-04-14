7ce0831495b7138dd91c6b05eb26efdd3d15fa6c
илья <Oksimonov@mail.ru>
2025-04-14 21:20:18 +0300

    инициализируем класс

--- module_2/lab_2/lab_2.cpp
+++ module_2/lab_2/lab_2.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+
+class RomanNumeralHashTable {
+    private:
+        struct TableEntry {
+            std::string key;
+            int value;
+            bool is_occupied;
+            bool is_deleted;
+    
+            TableEntry() : key(""), value(0), is_occupied(false), is_deleted(false) {}
+        };
+    
+        static const size_t INITIAL_CAPACITY = 16; //принадлежит классу в целом
+        static const double MAX_LOAD_FACTOR;
+        
+        TableEntry* entries;
+        size_t capacity;
+        size_t element_count;
+};
+
+
+
+
 int main() {
     setlocale(LC_ALL, "ru_RU");
     try {
